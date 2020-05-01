3295c36b29d4387571b3936de165bf36c38721b1
Dan Siegel <me@dansiegel.net>
2020-04-30 18:48:06 -0700

    adding markup extensions

---
+++ tests/AP.MobileToolkit.Fonts.Tests/CsharpMarkupTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AP.MobileToolkit.Fonts.Tests.Mocks;
+using AP.MobileToolkit.Markup;
+using Xamarin.Forms;
+using Xunit;
+
+namespace AP.MobileToolkit.Fonts.Tests
+{
+    public class CsharpMarkupTests
+    {
+        public CsharpMarkupTests()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            FontRegistry.Clear();
+            FontRegistry.RegisterFonts(SampleFontAwesomeRegular.Font, SampleFontAwesomeSolid.Font);
+        }
+
+        [Fact]
+        public void SetIconSetsTextAndFontFamilyOnLabel()
+        {
+            var label = new Label().SetIcon("far fa-calendar");
+
+            // "far fa-calendar", "\uf133"
+            Assert.Equal(SampleFontAwesomeRegular.FontFile, label.FontFamily);
+            Assert.Equal("\uf133", label.Text);
+        }
+
+        [Fact]
+        public void SetIconSetsTextAndFontFamilyOnSpan()
+        {
+            var label = new Span().SetIcon("far fa-calendar");
+
+            // "far fa-calendar", "\uf133"
+            Assert.Equal(SampleFontAwesomeRegular.FontFile, label.FontFamily);
+            Assert.Equal("\uf133", label.Text);
+        }
+
+        [Fact]
+        public void SetIconSetsTextAndFontFamilyOnEditor()
+        {
+            var label = new Editor().SetIcon("far fa-calendar");
+
+            // "far fa-calendar", "\uf133"
+            Assert.Equal(SampleFontAwesomeRegular.FontFile, label.FontFamily);
+            Assert.Equal("\uf133", label.Text);
+        }
+
+        [Fact]
+        public void SetIconSetsTextAndFontFamilyOnButton()
+        {
+            var label = new Button().SetIcon("far fa-calendar");
+
+            // "far fa-calendar", "\uf133"
+            Assert.Equal(SampleFontAwesomeRegular.FontFile, label.FontFamily);
+            Assert.Equal("\uf133", label.Text);
+        }
+
+        [Fact]
+        public void SetIconSetsTextAndFontFamilyOnSearchBar()
+        {
+            var label = new SearchBar().SetIcon("far fa-calendar");
+
+            // "far fa-calendar", "\uf133"
+            Assert.Equal(SampleFontAwesomeRegular.FontFile, label.FontFamily);
+            Assert.Equal("\uf133", label.Text);
+        }
+    }
+}
