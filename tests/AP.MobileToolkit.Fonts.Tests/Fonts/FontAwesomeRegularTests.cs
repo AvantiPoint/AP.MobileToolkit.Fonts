0190a7db68f4643aa5e6f5e5f3a65903b5904085
Dan Siegel <me@dansiegel.net>
2021-06-07 07:12:53 -0700

    adding Font validation tests

---
+++ tests/AP.MobileToolkit.Fonts.Tests/Fonts/FontAwesomeRegularTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AP.MobileToolkit.Fonts.Tests.Fonts
+{
+    public class FontAwesomeRegularTests : FontTestBase
+    {
+        protected override IFont Font => FontAwesomeRegular.Font;
+
+        protected override string FileName => "fa-regular-400.ttf";
+        protected override string Alias => "far";
+
+        [Theory]
+        [InlineData(Mappings.FontAwesomeRegular.User)]
+        public void ReturnsCorrectFont(string selector)
+        {
+            ReturnsCorrectFontInternal(selector);
+        }
+
+        [Theory]
+        [InlineData(Mappings.FontAwesomeRegular.User, "\uf007")]
+        public void MapsToGlyph(string mapping, string expectedGlyph)
+        {
+            MapsToGlyphInternal(mapping, expectedGlyph);
+        }
+    }
+}
---
+++ tests/AP.MobileToolkit.Fonts.Tests/Fonts/FontAwesomeSolidTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AP.MobileToolkit.Fonts.Tests.Fonts
+{
+    public class FontAwesomeSolidTests : FontTestBase
+    {
+        protected override IFont Font => FontAwesomeSolid.Font;
+
+        protected override string FileName => "fa-solid-900.ttf";
+        protected override string Alias => "fas";
+
+        [Theory]
+        [InlineData(Mappings.FontAwesomeSolid.User)]
+        public void ReturnsCorrectFont(string selector)
+        {
+            ReturnsCorrectFontInternal(selector);
+        }
+
+        [Theory]
+        [InlineData(Mappings.FontAwesomeSolid.User, "\uf007")]
+        public void MapsToGlyph(string mapping, string expectedGlyph)
+        {
+            MapsToGlyphInternal(mapping, expectedGlyph);
+        }
+    }
+}
