f3eecffa9d0f17ea0b980d59b3a4af5ce9f2c4a4
Dan Siegel <me@dansiegel.net>
2021-06-07 07:56:10 -0700

    Add DevIcons and Material Icons

---
+++ tests/AP.MobileToolkit.Fonts.Tests/Fonts/DevIconsTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AP.MobileToolkit.Fonts.Tests.Fonts
+{
+    public class DevIconsTests : FontTestBase
+    {
+        protected override IFont Font => DevIcons.Font;
+
+        protected override string FileName => "devicons.ttf";
+        protected override string Alias => "devicons";
+
+        [Theory]
+        [InlineData(Mappings.DevIcons.Android)]
+        public void ReturnsCorrectFont(string selector)
+        {
+            ReturnsCorrectFontInternal(selector);
+        }
+
+        [Theory]
+        [InlineData(Mappings.DevIcons.Android, "\ue60e")]
+        public void MapsToGlyph(string mapping, string expectedGlyph)
+        {
+            MapsToGlyphInternal(mapping, expectedGlyph);
+        }
+    }
+}
---
+++ tests/AP.MobileToolkit.Fonts.Tests/Fonts/MaterialIconsSharpTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace AP.MobileToolkit.Fonts.Tests.Fonts
+{
+    public class MaterialIconsSharpTests : FontTestBase
+    {
+        protected override IFont Font => MaterialIconsSharp.Font;
+
+        protected override string FileName => "MaterialIconsSharp-Regular.otf";
+        protected override string Alias => "material-sharp";
+
+        [Theory]
+        [InlineData(Mappings.MaterialIconsSharp.Android)]
+        public void ReturnsCorrectFont(string selector)
+        {
+            ReturnsCorrectFontInternal(selector);
+        }
+
+        [Theory]
+        [InlineData(Mappings.MaterialIconsSharp.Android, "\ue859")]
+        public void MapsToGlyph(string mapping, string expectedGlyph)
+        {
+            MapsToGlyphInternal(mapping, expectedGlyph);
+        }
+    }
+}
