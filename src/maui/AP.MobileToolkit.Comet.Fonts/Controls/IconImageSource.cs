2255b9fc9f00b99edd6376fd5d210a557017c28d
Dan Siegel <me@dansiegel.net>
2021-06-09 07:50:50 -0700

    add maui sample

---
+++ src/maui/AP.MobileToolkit.Comet.Fonts/Controls/IconImageSource.cs
@@ -0,0 +1,14 @@
+﻿using Comet;
+using Microsoft.Maui.Graphics;
+
+namespace AP.MobileToolkit.Fonts.Controls
+{
+    public class IconImageSource : ImageSource, IIconImageSource
+    {
+        public override bool IsEmpty => !string.IsNullOrEmpty(Name);
+
+        public string Name { get; set; }
+        public double Size { get; set; } = 12.0;
+        public Color Color { get; set; } = Colors.Black;
+    }
+}
