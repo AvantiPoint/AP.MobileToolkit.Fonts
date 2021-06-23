aeb0a0e546e5803b113d24cd281bdba032b2d0f0
Dan Siegel <me@dansiegel.net>
2021-06-23 08:41:56 -0700

    maui preview 5

---
+++ src/maui/AP.MobileToolkit.Maui.Fonts.Core/Controls/KnownPropertyNames.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace AP.MobileToolkit.Fonts.Controls
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class KnownPropertyNames
+    {
+        public const string Icon = nameof(Icon);
+
+        public const string Color = nameof(Color);
+
+        public const string Size = nameof(Size);
+    }
+}
--- src/maui/AP.MobileToolkit.Maui.Fonts/Controls/FontIcon.cs
+++ src/maui/AP.MobileToolkit.Maui.Fonts/Controls/FontIcon.cs
@@ -1,73 +1,17 @@
-﻿using System;
-using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Hosting;
 
 namespace AP.MobileToolkit.Fonts.Controls
 {
     public static class FontIcon
     {
-        public delegate void FontIconHandler(BindableObject bindable, string selector, string glyph, string fontFamily);
-
         public static readonly BindableProperty IconProperty =
-            BindableProperty.CreateAttached("Icon", typeof(string), typeof(BindableObject), null, propertyChanged: OnIconChanged);
+            BindableProperty.CreateAttached(KnownPropertyNames.Icon, typeof(string), typeof(BindableObject), null);
 
         public static readonly BindableProperty ColorProperty =
-            BindableProperty.CreateAttached("Color", typeof(Color), typeof(BindableObject), Colors.Black, propertyChanged: OnIconChanged);
+            BindableProperty.CreateAttached(KnownPropertyNames.Color, typeof(Color), typeof(BindableObject), Colors.Black);
 
         public static readonly BindableProperty SizeProperty =
-            BindableProperty.CreateAttached("Size", typeof(double), typeof(BindableObject), 12.0, propertyChanged: OnIconChanged);
-
-        private static FontIconHandler _defaultOnChanged = DefaultOnChanged;
-
-        private static void OnIconChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            string fontFamily = null;
-            string glyph = null;
-            string selector = (string)bindable.GetValue(IconProperty);
-            if (!string.IsNullOrEmpty(selector) && RegistryLocator.Registry is not null && RegistryLocator.Registry.HasFont(selector, out var font))
-            {
-                fontFamily = font.FontFileName;
-                glyph = font.GetGlyph(selector);
-            }
-
-            switch (bindable)
-            {
-                case Label label:
-                    label.Text = glyph;
-                    label.FontFamily = fontFamily;
-                    break;
-                case Button btn:
-                    btn.Text = glyph;
-                    btn.FontFamily = fontFamily;
-                    break;
-                case Span span:
-                    span.Text = glyph;
-                    span.FontFamily = fontFamily;
-                    break;
-                case MenuItem menuItem:
-                    menuItem.IconImageSource = CreateIconImageSource(bindable, selector);
-                    break;
-                default:
-                    _defaultOnChanged?.Invoke(bindable, selector, glyph, fontFamily);
-                    break;
-            }
-        }
-
-        public static void RegisterDefaultOnChangedHandler(FontIconHandler fontIconHandler) =>
-            _defaultOnChanged = fontIconHandler;
-
-        public static IconImageSource CreateIconImageSource(BindableObject bindable, string selector) =>
-            new()
-            {
-                Name = selector,
-                Size = (double)bindable.GetValue(SizeProperty),
-                Color = (Color)bindable.GetValue(ColorProperty)
-            };
-
-        private static void DefaultOnChanged(BindableObject bindable, string selector, string glyph, string fontFamily)
-        {
-            Console.WriteLine($"The control type '{bindable.GetType().FullName}' is not supported. To add support please register a delegate handler or file an issue on GitHub.");
-        }
+            BindableProperty.CreateAttached(KnownPropertyNames.Size, typeof(double), typeof(BindableObject), 12.0);
     }
 }
