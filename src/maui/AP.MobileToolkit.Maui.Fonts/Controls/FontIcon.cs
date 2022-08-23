94dce08f6d6085e857e14099d49cea8ec81ac170
Dan Siegel <me@dansiegel.net>
2022-08-23 07:27:28 -0700

    fixing FontIcon getter/setter

--- src/maui/AP.MobileToolkit.Maui.Fonts/Controls/FontIcon.cs
+++ src/maui/AP.MobileToolkit.Maui.Fonts/Controls/FontIcon.cs
@@ -13,5 +13,23 @@ namespace AP.MobileToolkit.Fonts.Controls
 
         public static readonly BindableProperty SizeProperty =
             BindableProperty.CreateAttached(KnownPropertyNames.Size, typeof(double), typeof(BindableObject), 12.0);
+
+        public static string GetIcon(BindableObject bindable) =>
+            (string)bindable.GetValue(IconProperty);
+
+        public static void SetIcon(BindableObject bindable, string icon) =>
+            bindable.SetValue(IconProperty, icon);
+
+        public static Color GetColor(BindableObject bindable) =>
+            (Color)bindable.GetValue(ColorProperty);
+
+        public static void SetColor(BindableObject bindable, Color color) =>
+            bindable.SetValue(ColorProperty, color);
+
+        public static double GetSize(BindableObject bindable) =>
+            (double)bindable.GetValue(SizeProperty);
+
+        public static void SetSize(BindableObject bindable, double size) =>
+            bindable.SetValue(SizeProperty, size);
     }
 }
